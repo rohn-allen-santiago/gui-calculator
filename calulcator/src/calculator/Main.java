546bfa413e0036b68682a2467327202087def6ca
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2023-11-29 16:19:31 +0000

    created initial main class and initialized jframe

--- calulcator/src/calculator/Main.java
+++ calulcator/src/calculator/Main.java
@@ -1,4 +1,24 @@
 package calculator;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static final int FRAME_X = 500;
+    public static final int FRAME_Y = 0;
+    public static final int FRAME_WIDTH = 600;
+    public static final int FRAME_HEIGHT = 800;
+
+    public static void main(String[] args) {
+
+        // Create JFrame
+        JFrame frame = new JFrame("Calculator");
+        frame.setVisible(true);
+
+        // Set exit behaviour
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+
+        // Set position and size of frame
+        frame.setBounds(FRAME_X, FRAME_Y, FRAME_WIDTH, FRAME_HEIGHT);
+    }
 }
