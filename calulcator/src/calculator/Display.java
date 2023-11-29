c41144069eb4c7c92eb14615fb088281fd12bf9d
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2023-11-29 16:50:14 +0000

    adjusted text display alignment and added updateText method for Display

--- calulcator/src/calculator/Display.java
+++ calulcator/src/calculator/Display.java
@@ -8,6 +8,8 @@ public class Display {
 
     public Display() {
         display = new JLabel();
+        display.setHorizontalAlignment(JLabel.LEFT);
+        display.setVerticalAlignment(JLabel.CENTER);
     }
 
     public JLabel getDisplay() {
@@ -16,5 +18,10 @@ public class Display {
 
     public void setDisplay(JLabel display) {
         this.display = display;
+    }
+
+    public void updateText(String symbol) {
+        String currentText = display.getText();
+        display.setText(currentText + symbol);
     }
 }
