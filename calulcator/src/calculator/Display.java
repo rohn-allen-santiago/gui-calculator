67f0355f8e365b2d9e6981f13afff22047ba534a
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2024-01-27 18:01:47 +0000

    Added button panel and buttons

--- calulcator/src/calculator/Display.java
+++ calulcator/src/calculator/Display.java
@@ -5,28 +5,50 @@ import java.awt.*;
 
 public class Display {
 
-    public static final int FRAME_X = 500;
-    public static final int FRAME_Y = 0;
     public static final int FRAME_WIDTH = 600;
     public static final int FRAME_HEIGHT = 800;
+    public static final int BUTTON_PANEL_X = 0;
+    public static final int BUTTON_PANEL_Y = 170;
+    public static final int BUTTON_PANEL_WIDTH = 600;
+    public static final int BUTTON_PANEL_HEIGHT = 600;
 
-   Display() {
+    String[] buttonChars = {"7", "8", "9", "/",
+                            "4", "5", "6", "*",
+                            "1", "2", "3", "-",
+                            "0", "C", "=", "+"};
+    JButton[] buttons = new JButton[buttonChars.length];
 
-        // Create JFrame
-        JFrame frame = new JFrame("Calculator");
+    JFrame frame;
+
+    // Constructor for creating calculator
+    Display() {
+        this.frame = new JFrame("Calculator");
+    }
+
+    // Function for initializing calculator
+    public void calculator() {
+
+        // Set JFrame parameters
         frame.setVisible(true);
-
-        // Set exit behaviour
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
-        // Set size of frame
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-
-        // Sets frame to center of the screen
+        frame.setLayout(null);
         frame.setLocationRelativeTo(null);
 
-        // Set grid layout of frame
-        frame.setLayout(new GridLayout(4,4));
+        // Create button panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBounds(BUTTON_PANEL_X, BUTTON_PANEL_Y, BUTTON_PANEL_WIDTH, BUTTON_PANEL_HEIGHT);
+        buttonPanel.setLayout(new GridLayout(4,4));
+        buttonPanel.setBackground(Color.black);
+        frame.add(buttonPanel);
 
+        // Add buttons to grid
+        for(int i = 0; i < buttons.length; i++) {
+            JButton b = new JButton(buttonChars[i]);
+            b.setBackground(Color.black);
+            b.setForeground(Color.white);
+            buttons[i] = b;
+            buttonPanel.add(b);
+        }
     }
 }
